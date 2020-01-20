3b29ec278d024cec3f9f42141596023373820b85
Rick Glascock <rixong@gmail.com>
2020-01-20 22:41:20 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,7 @@ require "yaml"
 def load_library(library)
   emoticon_lib = {}
   db = YAML.load_file(library).each_pair { | k,v | 
-    emoticon_lib[k] = [get_meaning:v[0], get_emoticon:v[1]]
+    emoticon_lib[k] => [get_meaning:v[0], get_emoticon:v[1]]
   }
   
   pp emoticon_lib
